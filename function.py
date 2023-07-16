2f1c3e9ba447710839411df2bc42f5f61dd31484
husna <h111126522@gmail.com>
2023-07-17 01:36:14 +0500

    my 7th repo

---
+++ function.py
@@ -0,0 +1,36 @@
+# days of the year
+def multiply():
+    x = 12
+    y = 30
+    print(x*y)
+multiply()    
+
+# number of weeks in year
+def divide():
+    x = 365
+    y = 7
+    print(x/y)
+divide()
+
+def subtract():
+    x = ("work hours in a day:")
+    y = 24
+    z = 18 
+    print(x)
+    print(y-z)
+subtract()
+
+def addition():
+    x = ("lesson hours in a month:")
+    y = (2+2+2)
+    z = 4
+    print(x,y*z)
+addition()
+
+def add():
+    x = ("converting the year from Hijri to AD")
+    y = 1382
+    z = 621
+    print(y+z)
+add()
+    
